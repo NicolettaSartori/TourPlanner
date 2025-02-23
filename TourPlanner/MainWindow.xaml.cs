263b5c94da38bbeec4511fdce1fb2d5e04b63e55
Nicoletta Sarzi Sartori <shopping.trip01@gmail.com>
2025-02-23 19:32:33 +0100

    Add UI

--- TourPlanner/MainWindow.xaml.cs
+++ TourPlanner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,27 @@ namespace TourPlanner;
 /// </summary>
 public partial class MainWindow : Window
 {
+    public ObservableCollection<RowData> TableData { get; set; }
+    
     public MainWindow()
     {
         InitializeComponent();
+        
+        TableData = new ObservableCollection<RowData>
+        {
+            new RowData { Date = "Row 1 - A", Duration = "Row 1 - B", Distance = "Row 1 - C" },
+            new RowData { Date = "Row 2 - A", Duration = "Row 2 - B", Distance = "Row 2 - C" },
+            new RowData { Date = "Row 3 - A", Duration = "Row 3 - B", Distance = "Row 3 - C" },
+            new RowData { Date = "Row 4 - A", Duration = "Row 4 - B", Distance = "Row 4 - C" }
+        };
+
+        DataContext = this;
     }
+}
+
+public class RowData
+{
+    public string Date { get; set; }
+    public string Duration { get; set; }
+    public string Distance { get; set; }
 }
