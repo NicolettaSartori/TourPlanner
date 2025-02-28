f71d6393305947609f3dfef477950302ac90bdf6
Nicoletta Sarzi Sartori <shopping.trip01@gmail.com>
2025-02-28 15:38:27 +0100

    Add property binding for navbar text

--- TourPlanner/Views/Navbar.xaml.cs
+++ TourPlanner/Views/Navbar.xaml.cs
@@ -9,4 +9,27 @@ public partial class Navbar : UserControl
     {
         InitializeComponent();
     }
+
+    public static readonly DependencyProperty MenuItemsProperty =
+        DependencyProperty.Register(nameof(MenuItems), typeof(string[]), typeof(Navbar),
+            new PropertyMetadata(null, OnMenuItemsChanged));
+
+    public string[] MenuItems
+    {
+        get => (string[])GetValue(MenuItemsProperty);
+        set => SetValue(MenuItemsProperty, value);
+    }
+    public string[] ProcessedMenuItems { get; private set; } = [];
+    public int CountColumns { get; private set; }
+
+    private static void OnMenuItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (Navbar)d;
+        string[] processedItems = (e.NewValue as string[])?
+            // replaces "" with an empty string, because it's not possible to do a real empty string <sys:String>
+            .Select(s => s.Equals("\"\"") ? "" : s)
+            .ToArray() ?? [];
+        control.ProcessedMenuItems = processedItems;
+        control.CountColumns = processedItems.Length;
+    }
 }
--- TourPlanner/obj/Debug/net9.0-windows/TourPlanner.AssemblyInfo.cs
+++ TourPlanner/obj/Debug/net9.0-windows/TourPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505748f0d866116fda97837f7552a7ab84174a3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c064bd49c207c6b26bfa71e3912957ae9cd44e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
