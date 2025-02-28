a644c2ad54575d3ab21f92799020a7ba7de1b35f
Nicoletta Sarzi Sartori <shopping.trip01@gmail.com>
2025-02-28 16:42:46 +0100

    Add FontAwesome search icon

--- TourPlanner/obj/Debug/net9.0-windows/TourPlanner.AssemblyInfo.cs
+++ TourPlanner/obj/Debug/net9.0-windows/TourPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c064bd49c207c6b26bfa71e3912957ae9cd44e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71d6393305947609f3dfef477950302ac90bdf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
