f3097d49043e7fc5daa8b1c5884f4ee6d73b8dc0
Nicoletta Sarzi Sartori <shopping.trip01@gmail.com>
2025-02-28 19:28:06 +0100

    Create more user controls

--- TourPlanner/MainWindow.xaml.cs
+++ TourPlanner/MainWindow.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TourPlanner;
 
@@ -17,27 +9,8 @@ namespace TourPlanner;
 /// </summary>
 public partial class MainWindow : Window
 {
-    public ObservableCollection<RowData> TableData { get; set; }
-    
     public MainWindow()
     {
         InitializeComponent();
-        
-        TableData = new ObservableCollection<RowData>
-        {
-            new RowData { Date = "Row 1 - A", Duration = "Row 1 - B", Distance = "Row 1 - C" },
-            new RowData { Date = "Row 2 - A", Duration = "Row 2 - B", Distance = "Row 2 - C" },
-            new RowData { Date = "Row 3 - A", Duration = "Row 3 - B", Distance = "Row 3 - C" },
-            new RowData { Date = "Row 4 - A", Duration = "Row 4 - B", Distance = "Row 4 - C" }
-        };
-
-        DataContext = this;
     }
-}
-
-public class RowData
-{
-    public string Date { get; set; }
-    public string Duration { get; set; }
-    public string Distance { get; set; }
 }
--- TourPlanner/obj/Debug/net9.0-windows/TourPlanner.AssemblyInfo.cs
+++ TourPlanner/obj/Debug/net9.0-windows/TourPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71d6393305947609f3dfef477950302ac90bdf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a644c2ad54575d3ab21f92799020a7ba7de1b35f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
