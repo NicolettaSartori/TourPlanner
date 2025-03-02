6e1e127ebae4e6264cab470ed6a170fd121da585
Nicoletta Sarzi Sartori <shopping.trip01@gmail.com>
2025-03-02 21:40:12 +0100

    Add usercontrols

---
+++ TourPlanner/Views/List.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TourPlanner.Views;
+
+public partial class List : UserControl
+{
+    public List()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TourPlanner/Views/Map.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TourPlanner.Views;
+
+public partial class Map : UserControl
+{
+    public Map()
+    {
+        InitializeComponent();
+    }
+}
